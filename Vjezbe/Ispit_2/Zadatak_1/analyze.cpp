585c46cf3ed603800ffeff490b0de9a28d3ef637
rbarac1 <rbarac@pmfst.hr>
2023-03-24 09:22:46 +0100

    Zadatak_1_corr

--- Vjezbe/Ispit_2/Zadatak_1/analyze.cpp
+++ Vjezbe/Ispit_2/Zadatak_1/analyze.cpp
@@ -3,9 +3,9 @@ int main()
 {
   Analyzer* analyzer = new Analyzer();
   float a, b, c;
-  a = 5;
-  b = 4;
-  c = 3;
+  a = 7,1;
+  b = 5,4;
+  c = 3.01;
   analyzer->Evolume(a, b, c);
   analyzer->~Analyzer();
   return 0;
